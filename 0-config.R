6d5e53642a31848d19e68258c774e9601f64bc06
Tofu122 <136790724+Tofu122@users.noreply.github.com>
2024-06-07 11:09:06 +0800

    Add files via upload

--- 0-config.R
+++ 0-config.R
@@ -1,6 +1,6 @@
 
 #-------------------------------------
-# EE substudies analysis
+# EE substudies analysis 
 
 # configure data directories
 # source base functions
@@ -10,35 +10,42 @@
 library(tidyverse)
 library(haven)
 library(washb)
-library(tmle)
 library(foreign)
 library(data.table)
 library(devtools)
 library(kableExtra)
 library(here)
-library(boxr)
+library(mgcv)
+library(psych)
 
-box_auth()
+
+if(!require(faraway)){
+  install.packages("faraway")
+  library(faraway)
+}
+
+if(!require(washbgam)){
+  devtools::install_github("washb-eed-substudies/washbgam")
+  library(washbgam)
+}
 
 dropboxDir <- NULL
-if(dir.exists("C:/Users/andre/Dropbox/WASHB-EE-analysis/WBB-EE-analysis/")){
+if(dir.exists("C:/Users/andre/Dropbox/WASHB-EE-analysis/WBB-EE-analysis/")){ 
   dropboxDir <- "C:/Users/andre/Dropbox/WASHB-EE-analysis/WBB-EE-analysis/"
 }
-if(dir.exists("/Users/audrielin/Dropbox/WBB-EE-analysis/")){
+if(dir.exists("/Users/audrielin/Dropbox/WBB-EE-analysis/")){ 
   dropboxDir <- "/Users/audrielin/Dropbox/WBB-EE-analysis/"
 }
-if(dir.exists("/Users/sophiatan/Dropbox/WASH/")){
+if(dir.exists("/Users/sophiatan/Dropbox/WASH/")){ 
   dropboxDir <- "/Users/sophiatan/Dropbox/WASH/"
 }
-if(dir.exists("/Users/lisa/Dropbox/WASH/")){
+if(dir.exists("/Users/lisa/Dropbox/WASH/")){ 
   dropboxDir <- "/Users/lisa/Dropbox/WASH/"
 }
-if(dir.exists("/Users/caitlinhemlock/Dropbox/")){
-  dropboxDir <- "/Users/caitlinhemlock/"
-}
-if(dir.exists("/Users/zbutzindozier/Dropbox/WBB-EE-analysis/")){
-  dropboxDir <- "/Users/zbutzindozier/Dropbox/WBB-EE-analysis/"
-}
+
+library(boxr)
+#box_auth()
+#d <- box_read(871638120165) %>% filter(pregnancy_immune == 1)
 
 theme_ki<-function(){
   theme_bw() %+replace%
@@ -58,10 +65,19 @@ theme_set(theme_ki())
 tableau10 <- c("#1F77B4","#FF7F0E","#2CA02C","#D62728",
                "#9467BD","#8C564B","#E377C2","#7F7F7F",
                "#BCBD22","#17BECF")
-tableau11 <- c("black","#1F77B4","#FF7F0E","#2CA02C","#D62728",
-               "#9467BD","#8C564B","#E377C2","#7F7F7F",
-               "#BCBD22","#17BECF")
 
-#boxr setup
 
+#save R package versions
+
+# # Only run thise lines once when project is initialized 
+# #Call renv::init() to initialize a new project-local environment with a private R library,
+# renv::init(project=here()) 
+# 
+# # Only run thise line when packages are updated
+# #Call renv::snapshot() to save the state of the project library to the lockfile (called renv.lock),
+# renv::snapshot()
+# 
+# # Only run these lines when needed (upon initialization and then when package versions need to be restored)
+# #call renv::restore() to  revert to the previous state as encoded in the lockfile 
+# renv::restore()
 
---
+++ src/H2_adjusted.R
@@ -0,0 +1,84 @@
+rm(list=ls())
+
+# Load libraries and configuration
+source(here::here("0-config.R"))
+library(here)
+library(VIM)
+
+# Construct the path to the RDS file directly within the project folder
+data_path <- here("bangladesh-cleaned-master-data.RDS")
+
+# Read the data from the RDS file
+d <- readRDS(data_path)
+
+#Set list of adjustment variables
+#Make vectors of adjustment variable names
+Wvars<-c("sex","birthord", "momage","momheight","momedu","gest_age_weeks",
+         "hfiacat", "Nlt18","Ncomp", "watmin", "walls", "floor", "HHwealth_scaled",
+         "tr", "life_viol_any_t3_cat", "viol_any_preg_cat")
+
+#Add in time varying covariates:
+Wvars2 <- c(Wvars, c("ageday_bt2", "month_blood_t0", "month_bt2"))
+Wvars3 <- c(Wvars, c("ageday_bt3", "month_blood_t0", "month_bt3"))
+
+pick_covariates <- function(j){
+  # j is outcome as string
+  # choose correct adjustment set based on outcome
+  if(grepl("t2", j)){Wset = Wvars}
+  else{Wset = Wvars3}
+  return(Wset)
+}
+
+
+#Loop over exposure-outcome pairs
+
+##Hypothesis 2
+Xvars <- c("agp", "crp", "sumscore_t0_mom_Z")
+Yvars <- c("ln_L_conc_t1", "ln_M_conc_t1", "ln_mpo1", "ln_aat1", "ln_neo1", "ln_L_conc_t2", "ln_M_conc_t2", "ln_mpo2", "ln_aat2", "ln_neo2", "ln_reg2", "ln_L_conc_t3", "ln_M_conc_t3","ln_mpo3", "ln_aat3", "ln_neo3")
+
+#Fit models
+H2_adj_models <- NULL
+for(i in Xvars){
+  for(j in Yvars){
+    print(i)
+    print(j)
+    Wset<-pick_covariates(j)
+    res_adj <- fit_RE_gam(d=d, X=i, Y=j,  W=Wset)
+    res <- data.frame(X=i, Y=j, fit=I(list(res_adj$fit)), dat=I(list(res_adj$dat)))
+    H2_adj_models <- bind_rows(H2_adj_models, res)
+  }
+}
+
+
+
+#Get primary contrasts
+H2_adj_res <- NULL
+for(i in 1:nrow(H2_adj_models)){
+  res <- data.frame(X=H2_adj_models$X[i], Y=H2_adj_models$Y[i])
+  if(grepl("_def", H2_adj_models$X[i])){
+    preds <- predict_gam_diff(fit=H2_adj_models$fit[i][[1]], d=H2_adj_models$dat[i][[1]], quantile_diff=c(0.25,0.75), Xvar=res$X, Yvar=res$Y, binary=T)
+  }else{
+    preds <- predict_gam_diff(fit=H2_adj_models$fit[i][[1]], d=H2_adj_models$dat[i][[1]], quantile_diff=c(0.25,0.75), Xvar=res$X, Yvar=res$Y)
+  }
+  H2_adj_res <-  bind_rows(H2_adj_res , preds$res)
+}
+
+#Make list of plots
+H2_adj_plot_list <- NULL
+H2_adj_plot_data <- NULL
+for(i in 1:nrow(H2_adj_models)){
+  res <- data.frame(X=H2_adj_models$X[i], Y=H2_adj_models$Y[i])
+  simul_plot <- gam_simul_CI(H2_adj_models$fit[i][[1]], H2_adj_models$dat[i][[1]], xlab=res$X, ylab=res$Y, title="")
+  H2_adj_plot_list[[i]] <-  simul_plot$p
+  H2_adj_plot_data <-  rbind(H2_adj_plot_data, data.frame(Xvar=res$X, Yvar=res$Y, adj=0, simul_plot$pred %>% subset(., select = c(Y,X,id,fit,se.fit,uprP, lwrP,uprS,lwrS))))
+}
+
+# Save models
+saveRDS(H2_adj_models, file = here("H2_adj_models.RDS"))
+
+# Save results
+saveRDS(H2_adj_res, here("H2_adj_res.RDS"))
+
+# Save plot data
+saveRDS(H2_adj_plot_data, here("H2_adj_plot_data.RDS"))
+
